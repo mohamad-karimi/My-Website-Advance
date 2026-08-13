6d767a40c4069cd13ad16ba8e25042b6981120f4
mohamad-karimi <karimi.mohamad0011@gmail.com>
2026-08-13 10:06:04 +0330

    Token login and logout
    Make custom serializer and view for login and logout with token.

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from ...models import CustomUser
 import django.contrib.auth.password_validation as validators
 from django.core import exceptions
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import serializers
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(max_length=120, write_only = True)
@@ -32,3 +36,48 @@ class RegistrationSerializer(serializers.ModelSerializer):
         validated_data.pop("password2", None)
 
         return CustomUser.objects.create_user(**validated_data)
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(
+        label=_("Email"),
+        write_only=True
+    )
+
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+
+    token = serializers.CharField(
+        label=_("Token"),
+        read_only=True
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get('request'),
+                username=email,
+                password=password
+            )
+
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(
+                    msg,
+                    code='authorization'
+                )
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(
+                msg,
+                code='authorization'
+            )
+
+        attrs['user'] = user
+        return attrs
--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'api-v1'
 
 urlpatterns = [ 
-    path('registration', views.RegistrationApiView.as_view(), name="registration")
+    path('registration', views.RegistrationApiView.as_view(), name="registration"),
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name="token-login"),
+    path('token/logout/', views.CustomDestroyAuthToken.as_view(), name="token-logout"),
 ]
